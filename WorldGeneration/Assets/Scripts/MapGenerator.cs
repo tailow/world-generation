5747d970d0dc7839a674f8eb64fac98437c11f35
tailow-pc <revitate@hotmail.com>
2018-04-11 17:39:36 +0300

    Started flattening island edge

--- WorldGeneration/Assets/Scripts/MapGenerator.cs
+++ WorldGeneration/Assets/Scripts/MapGenerator.cs
@@ -11,6 +11,7 @@ public class MapGenerator : MonoBehaviour
     public int height;
     public float depth;
     public float scale;
+    public float edgeStartDistance;
 
     #endregion
 
@@ -40,10 +41,21 @@ public class MapGenerator : MonoBehaviour
         {
             for (int x = 0; x < width; x++)
             {
-                noiseMap[x, y] = Mathf.PerlinNoise((float)x / width * scale, (float)y / height * scale);
+                float pointHeightMultiplier = GetPointHeight(new Vector2(x, y));
+
+                noiseMap[x, y] = Mathf.PerlinNoise((float)x / width * scale, (float)y / height * scale) * pointHeightMultiplier;
             }
         }
 
         return noiseMap;
+    }
+
+    float GetPointHeight(Vector2 point)
+    {
+        Vector2 centerPoint = new Vector2(width / 2, height / 2);
+
+        float heightMultiplier = 1 - Mathf.Clamp(Vector2.Distance(point, centerPoint), 0f, 1f);
+
+        return heightMultiplier;
     }
 }
